48cb8780a54d1c6d46cf87d2bcea746edf0071e4
sob3r69 <iamnotsob3r@gmail.com>
2023-11-30 21:13:53 +0300

    feature: Added response headers

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,24 @@ import request from 'request';
 const app: Express = express();
 const port = 3000;
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.get('/', (req: Request, res: Response) => {
   request.get(
     { url: 'https://store.steampowered.com/api/appdetails?appids=632360', json: true },
@@ -11,14 +29,24 @@ app.get('/', (req: Request, res: Response) => {
       if (error) {
         res.send(error);
       } else {
-        res.send(body);
+        res.send(body[req.params.id].data);
       }
     }
   );
 });
 
-app.get('/test', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get('/game/:id', (req: Request, res: Response) => {
+  request.get(
+    { url: 'https://store.steampowered.com/api/appdetails?appids=' + req.params.id, json: true },
+    (error, response, body) => {
+      console.log(req.params);
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(body[req.params.id].data);
+      }
+    }
+  );
 });
 
 app.listen(port, () => {
